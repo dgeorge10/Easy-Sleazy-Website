790e49005baf70eed33ba8e87b49cf169827723e
Dennis George <dennisgeorge10@gmail.com>
2017-09-07 13:55:27 -0400

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){	
+
+	$('li').mouseenter(function(){
+		$(this).css('color', 'black');
+	});
+	$('li').mouseleave(function(){
+		$(this).css('color', 'grey');
+	});	
+	
+    //settings for slider
+    var width = 720;
+    var animationSpeed = 1000;
+    var pause = 3000;
+    var currentSlide = 1;
+
+    //cache DOM elements
+    var $slider = $('#slider');
+    var $slideContainer = $('.slides', $slider);
+    var $slides = $('.slide', $slider);
+
+    var interval;
+
+    function startSlider() {
+        interval = setInterval(function() {
+            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+        }, pause);
+    }
+    function pauseSlider() {
+        clearInterval(interval);
+    }
+
+    $slideContainer
+        .on('mouseenter', pauseSlider)
+        .on('mouseleave', startSlider);
+
+    startSlider();
+
+    $('#carousel .right').click(function(){
+    	$slideContainer.animate({'margin-left': '-='+width}, 'fast', function() {
+                if (++currentSlide === $slides.length) {
+                    currentSlide = 1;
+                    $slideContainer.css('margin-left', 0);
+                }
+            });
+    	clearInterval(interval);
+	});
+	
+});
